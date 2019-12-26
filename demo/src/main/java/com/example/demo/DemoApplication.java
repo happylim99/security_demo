7bc993b2c2a00cc4eab5bf6fed3bf2aef97369f7
SeanLim <happylim99@gmail.com>
2019-12-26 11:27:39 +0800

    complete

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import com.example.demo.jwt.JwtConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	/*
+	@Bean
+	public JwtConfig jwtConfig()
+	{
+		return new JwtConfig();
+	}
+	*/
+}
